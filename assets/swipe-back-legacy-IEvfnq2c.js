8d9747cedfb9fc8f4fe460d875f647f78bb0d0f1
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-03-07 19:26:10 +0800

    Updates

--- assets/swipe-back-legacy-IEvfnq2c.js
+++ assets/swipe-back-legacy-IEvfnq2c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3kxZOk2P.js","./index3-legacy-CBO-TNTE.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CCnvimkJ.js","./index3-legacy-C6zB-0G6.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
