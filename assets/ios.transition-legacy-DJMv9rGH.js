a7d790cda14af77d9d920ee3723ef537349e0fa9
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-03-25 19:19:49 +0800

    Updates

--- assets/ios.transition-legacy-DJMv9rGH.js
+++ assets/ios.transition-legacy-DJMv9rGH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B0qUh7KH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DdLymbDj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
