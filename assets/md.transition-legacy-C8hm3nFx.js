5b5d0ea9d41bcae03cdff8dd1ed69266f39775aa
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-04-04 09:25:10 +0800

    Updates

--- assets/md.transition-legacy-C8hm3nFx.js
+++ assets/md.transition-legacy-C8hm3nFx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DdLymbDj.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-tKSR0L8Z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
