e064555e4911d97ac0955fe2a6cee1abe21283ac
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-04-21 17:38:18 +0800

    added EditProfilePages.tsx

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dqjqlzdeouujrscjdipu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxanFsemRlb3V1anJzY2pkaXB1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzcyNzIwMCwiZXhwIjoyMDU5MzAzMjAwfQ.4rF2KkTYkOQSvcXMIgUpvBU4G6j6fwZ6AHsnJR62qFM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
